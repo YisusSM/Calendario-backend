ca23832b04d3d7bb4cdd8d4ee0efb9acad95f161
YisusSM <xxchuchoxd@outlook.com>
2021-09-01 23:19:29 -0500

    fix routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
-const path = require('path');
+
 
 //Crear server de express
 
@@ -18,12 +18,11 @@ app.use(cors());
 app.use(express.json());
 
 //rutas
+app.use('/login', express.static('public'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/events',require('./routes/events'));
 
-app.get('*', ( req, res ) => {
-    res.sendFile( path.join( __dirname+'/public/index.html' ) );
-});
+
 //CRUD:Eventos
 
 
