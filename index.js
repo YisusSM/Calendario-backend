dfc5330a4ac3db4f40c4edcee0d8789de0f5bf45
YisusSM <xxchuchoxd@outlook.com>
2021-09-01 21:40:28 -0500

    correcion de rutas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
+const path = require('path');
 
 //Crear server de express
 
@@ -19,6 +20,10 @@ app.use(express.json());
 //rutas
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/events',require('./routes/events'));
+
+app.get('*', ( req, res ) => {
+    res.sendFile( path.join( __dirname+'/public/index.html' ) );
+});
 //CRUD:Eventos
 
 
